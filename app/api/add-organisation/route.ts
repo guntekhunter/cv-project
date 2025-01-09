af758def65e4888d57dbd635ce18f7d88d4eb120
guntekhunter <guntek2000@gmail.com>
2025-01-09 22:10:38 +0800

    feat: new api -> add organisation

---
+++ app/api/add-organisation/route.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const reqBody = await req.json();
+
+  try {
+    const newOrganisation = await prisma.organisation.create({
+      data: {
+        organisation_name: reqBody.organisation_name,
+        address: reqBody.address,
+        responsibility: reqBody.responsibility,
+        division: reqBody.division,
+        type: reqBody.type,
+        start_date: new Date(reqBody.start_date), // Convert to Date object
+        end_date: new Date(reqBody.end_date),
+        cv_id: reqBody.cv_id,
+      },
+    });
+    return NextResponse.json({ data: newOrganisation });
+  } catch (err) {
+    return NextResponse.json({ err });
+  }
+}
