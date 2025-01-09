18f1ea15890fddf2407f2b1654e22dd5aad3e92b
guntekhunter <guntek2000@gmail.com>
2025-01-09 22:11:12 +0800

    feat: new api -> add personal data

---
+++ app/api/add-personal-data/route.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const reqBody = await req.json();
+
+  try {
+    const newOther = await prisma.personalData.create({
+      data: {
+        link: reqBody.link,
+        portfolio: reqBody.name,
+        address: reqBody.address,
+        professional_summary: reqBody.professional_summary,
+        photo: reqBody.photo,
+        name: reqBody.name,
+        cv_id: reqBody.cv_id,
+      },
+    });
+    return NextResponse.json({ data: newOther });
+  } catch (err) {
+    return NextResponse.json({ err });
+  }
+}
