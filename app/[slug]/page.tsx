d78baef432ae1b0e5ca0623f06b5c5a2aa2815ac
guntekhunter <guntek2000@gmail.com>
2025-07-31 23:03:04 +0800

    fix: article slug to client

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,22 +1,33 @@
-import Link from "next/link";
-import { ArrowLeftIcon } from "@heroicons/react/24/solid";
-import { getArticleData, getSortedArticles } from "@/lib/article";
+"use client";
 
-type PageProps = {
-  params: {
-    slug: string;
-  };
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import { getArticles } from "../fetch/get/fetch";
+
+type ArticleData = {
+  title: string;
+  date: string;
+  constHtml: string;
 };
 
-export async function generateStaticParams() {
-  const articles = await getSortedArticles();
-  return articles.map((article) => ({
-    slug: article.id,
-  }));
-}
+export default function Article() {
+  const { slug } = useParams();
+  const [articleData, setArticleData] = useState<ArticleData | null>(null);
+  const [loading, setLoading] = useState(true);
 
-const Article = async ({ params }: PageProps) => {
-  const articleData = await getArticleData(params.slug);
+  useEffect(() => {
+    if (!slug) return;
+    const fetchData = async () => {
+      const res = await getArticles(slug);
+      setArticleData(res?.data.data);
+      setLoading(false);
+    };
+    fetchData();
+  }, [slug]);
+
+  if (loading || !articleData) return <div className="p-10">Loading...</div>;
 
   return (
     <section className="w-full flex justify-center md:py-[7rem] py-[5rem]">
@@ -30,9 +41,7 @@ const Article = async ({ params }: PageProps) => {
         <h1 className="font-bold md:text-[3rem] text-[1.6rem]">
           {articleData.title}
         </h1>
-        <p className="text-[.8rem] text-gray-500 italic">
-          {articleData.date.toString()}
-        </p>
+        <p className="text-[.8rem] text-gray-500 italic">{articleData.date}</p>
         <article
           className="prose prose-zinc max-w-none text-[.7rem] md:text-base"
           dangerouslySetInnerHTML={{ __html: articleData.constHtml }}
@@ -40,6 +49,4 @@ const Article = async ({ params }: PageProps) => {
       </div>
     </section>
   );
-};
-
-export default Article;
+}
