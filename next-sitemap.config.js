77a9d100b00cd5ffdaac332cade19035da990e98
guntekhunter <guntek2000@gmail.com>
2025-07-31 23:39:45 +0800

    fead: robot.txs and sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.buatcv.id/",
+  generateRobotsTxt: true,
+  // optionally exclude pages
+  // exclude: ['/secret-page'],
+};
