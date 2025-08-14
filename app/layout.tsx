f29aae49c695313df14c85989ea6367ffeb52f8f
Muh. Agung Haeruddin <72636466+guntekhunter@users.noreply.github.com>
2025-08-14 14:53:07 +0800

    fix: the analythict tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import Footer from "./component/global/Footer";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const openSans = Open_Sans({
   variable: "--font-open-sans",
@@ -42,33 +43,48 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta
-        name="google-site-verification"
-        content="sWKKEh-OTQGoUeA4XF90Z-Dg0GWmxPBreOdTe6iDQSc"
-      />
+        <meta
+          name="google-site-verification"
+          content="sWKKEh-OTQGoUeA4XF90Z-Dg0GWmxPBreOdTe6iDQSc"
+        />
 
-        <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');</script>
-<!-- End Google Tag Manager -->
-      
+        {/* Google Tag Manager Script */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${GeistSans.variable} ${GeistMono.variable} antialiased bg-[#FBFBFB] font-sans relative`}
       >
-        <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src={`https://www.googletagmanager.com/ns.html?id==${process.env.NEXT_PUBLIC_GTM_ID}`}
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Analytics />
         <Navbar />
         {children}
         <Footer />
+
+        {/* Google Analytics */}
+        <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS || ""} />
       </body>
-      <GoogleAnalytics gaId=${process.env.GOOGLE_ANALYTICS} />
     </html>
   );
 }
